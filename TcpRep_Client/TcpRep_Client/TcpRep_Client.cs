b7b824cece4a1ad14eec8fd753f8d35a49245039
hikmatul ulya <72594758+hikmah-ulya@users.noreply.github.com>
2021-12-23 14:30:55 +0700

    Add files via upload

---
+++ TcpRep_Client/TcpRep_Client/TcpRep_Client.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using StringLibrary;
+using System.Threading;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TcpRep_Client
+{
+	class Reader
+	{
+		public void ReadMessage(object obj)
+		{
+			TcpClient tcpClient = (TcpClient)obj;
+			StreamReader streamReader = new StreamReader(tcpClient.GetStream());
+
+			while (true)
+			{
+				try
+				{
+
+					string message = streamReader.ReadLine();
+					Console.WriteLine(message);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					break;
+				}
+			}
+
+			try
+			{
+				TcpClient tcpClient = new TcpClient("127.0.0.1", 5000);
+
+				Thread thread = new Thread(reader.ReadMessage);
+				thread.Start(tcpClient);
+				NetworkStream networkStream = tcpClient.GetStream();
+				Player player = new Player();
+				while (true)
+				{
+					if (tcpClient.Connected)
+					{
+						Console.WriteLine("Welcome Player");
+						BinaryFormatter binaryFormatter = new BinaryFormatter();
+						string input;
+						//reply here
+						Console.WriteLine("What's your name?");
+						input = Console.ReadLine();
+						player.name = input;
+						Console.WriteLine("What's your class?");
+						input = Console.ReadLine();
+						player.className = input;
+						//confirmation
+						binaryFormatter.Serialize(networkStream, player);
+
+						Console.ReadLine();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+		}
+	}
+
+		
+class Program
+    {
+
+		static void Main(string[] args)
+		{
+		Reader reader = new Reader();
+	}
+	}
+
+}
+}
---
+++ Tcprep_Server/Tcprep_Server/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using StringLibrary;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Tcprep_Server
+{
+        class HandleClient
+        {
+            Program program = new Program();
+            public void BroadCast(string msg)
+            {
+                foreach (TcpClient client in Program.tcpClientsList)
+                {
+          
+                    StreamWriter sWriter = new StreamWriter(client.GetStream());
+                    sWriter.WriteLine(msg);
+                    sWriter.Flush();
+                }
+            }
+
+            public void ClientListener(object obj)
+            {
+                TcpClient tcpClient = (TcpClient)obj;
+                
+                while (true)
+                {
+
+                    try
+                    {
+                        Player player = new Player();
+                        BinaryFormatter binaryFormatter = new BinaryFormatter();
+                        player = (Player)binaryFormatter.Deserialize(tcpClient.GetStream());
+                        BroadCast("Hello " + player.name);
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Player player = new Player();
+                        Console.WriteLine(e.Message);
+                        program.removeClient(tcpClient);
+                        BroadCast(player.name + "leave");
+                        break;
+                    }
+                }
+            }
+        }
+     
+    
+    class Program
+    {
+        public static TcpListener tcpListener;
+        public static List<TcpClient> tcpClientsList = new List<TcpClient>();
+
+        static void Main(string[] args)
+        {
+            HandleClient handleClient = new HandleClient();
+
+            //start process
+            tcpListener = new TcpListener(IPAddress.Any, 5000);
+            tcpListener.Start();
+            Console.WriteLine("Server created");
+            while (true)
+            {
+                //add clients to list
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                tcpClientsList.Add(tcpClient);
+
+                //start listener
+                Thread startListen = new Thread(() => handleClient.ClientListener(tcpClient));
+                startListen.Start();
+            }
+        }
+
+         public void removeClient(TcpClient client)
+         {
+            tcpClientsList.Remove(client);
+         }
+    }
+}
